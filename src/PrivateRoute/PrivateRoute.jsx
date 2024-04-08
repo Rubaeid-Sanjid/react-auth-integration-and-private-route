8b88aadd242ad54f1b227efeaa8e4dec072fa476
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-04-09 01:41:00 +0600

    added loader to avoid reloading from another page

--- src/PrivateRoute/PrivateRoute.jsx
+++ src/PrivateRoute/PrivateRoute.jsx
@@ -4,8 +4,11 @@ import { Navigate } from "react-router-dom";
 import PropTypes from "prop-types";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
 
+  if(loading){
+    return <span className="loading loading-spinner loading-lg"></span>
+  }
   if (user) {
     return children; //children is 'order' component
   }
